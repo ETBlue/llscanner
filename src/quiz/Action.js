61c4614f07897eaafdb915a6d1ae39cbfb949796
ETBlue <etblue.tw@gmail.com>
2017-03-12 01:13:50 +0800

    shorten action buttons

--- src/quiz/Action.js
+++ src/quiz/Action.js
@@ -11,9 +11,9 @@ class Action extends Component {
           <Button onClick={this.props.onToggle} icon="pencil" color="" />
         </div>
         <div className={"ui mini buttons " + this.props.editMode}>
-          <Button onClick={this.props.onSave} icon="checkmark" color="olive" className="labeled" title="確定送出" />
-          <Button onClick={this.props.onToggle} icon="cancel" color="" className="labeled" title="取消編輯" />
-          <Button onClick={this.props.onRefresh} icon="refresh" color="yellow" className="labeled" title="還原表單" />
+          <Button onClick={this.props.onSave} icon="checkmark" color="olive" className="labeled" title="送出" />
+          <Button onClick={this.props.onToggle} icon="cancel" color="" className="labeled" title="取消" />
+          <Button onClick={this.props.onRefresh} icon="refresh" color="yellow" className="labeled" title="還原" />
           {/*<Button onClick={this._delete} icon="trash" color="orange" />*/}
         </div>
       </div>
