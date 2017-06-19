c4d74c4d15ae8414eb7de9f5f3e0099af2ce6a00
ETBlue <etblue.tw@gmail.com>
2017-06-19 22:30:27 +0800

    rename laws

---
+++ src/law/_laws.js
@@ -0,0 +1,13 @@
+import law01139 from './json/勞動基準法.json'
+import law01143 from './json/勞資爭議處理法.json'
+import law03626 from './json/大量解僱勞工保護法.json'
+import law01132 from './json/工會法.json'
+import law02036 from './json/船員法.json'
+
+export default {
+  '勞動基準法': law01139,
+  '勞資爭議處理法': law01143,
+  '大量解僱勞工保護法': law03626,
+  '工會法': law01132,
+  '船員法': law02036,
+}
