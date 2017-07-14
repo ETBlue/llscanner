48655cdc2d296e65486bdefdf6b44394bc996422
ETBlue <etblue.tw@gmail.com>
2017-07-14 15:59:34 +0800

    extract scan result and add filter for it

--- src/answer/AnswerView.js
+++ src/answer/AnswerView.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import {HashLink as Link} from 'react-router-hash-link'
 
-import _evaluateCondition from './_evaluateCondition'
-
-import './AnswerView.css'
+import ResultView from './ResultView'
 
 class AnswerView extends Component {
 
@@ -12,11 +10,6 @@ class AnswerView extends Component {
     const answerData = this.props.answerData
     const rulesData = this.props.law['勞動基準法']
     const lawObject = this.props.lawObject
-    let count = {
-      'passed': 0,
-      'failed': 0,
-      'NA': 0
-    }
 
     let answerDataJSX
     if (answerData) {
@@ -36,79 +29,6 @@ class AnswerView extends Component {
       });
     }
 
-    let resultJSX
-    if (answerData && rulesData) {
-      resultJSX = Object.keys(rulesData).map((key) => {
-
-        if (key === '0') {
-          return
-        }
-
-        return rulesData[key].map((ruleSet, index) => {
-          if (!ruleSet) {
-            return
-          }
-
-          let evaluateConditionJSX
-          let hintJSX
-
-          if (ruleSet.precondition && _evaluateCondition(ruleSet.precondition, answerData) !== 'passed') {
-
-            evaluateConditionJSX = '不適用'
-            count.NA += 1
-
-          } else {
-
-            if (_evaluateCondition(ruleSet.condition, answerData) === 'passed') {
-              evaluateConditionJSX = <span className='ui green basic tiny label'><i className='icon checkmark' />通過</span>
-              count.passed += 1
-            }
-
-            if (_evaluateCondition(ruleSet.condition, answerData) === 'failed') {
-              evaluateConditionJSX = <span className='ui red tiny label'><i className='icon remove' />不通過</span>
-              count.failed += 1
-            }
-
-            const targetListJSX = ruleSet.condition.rule.map((item, number, arr) => {
-              return (
-                <div className='item' key={number}>
-                <span className='code'>
-                  {item.target}
-                </span>
-                </div>
-              )
-            })
-
-            hintJSX = (
-              <div className='ui list'>
-                {targetListJSX}
-              </div>
-            )
-          }
-
-          return (
-            <div className='item' key={index}>
-              <div className='content'>
-                <header className='header'>
-                  勞動基準法第 {key} 條
-                  { ruleSet.reference.paragraph.length > 0 ? `第 ${ruleSet.reference.paragraph} 項` : ''}
-                  { ruleSet.reference.subparagraph.length > 0 ? `第 ${ruleSet.reference.subparagraph} 款` : ''}
-                </header>
-                <p className='meta'>
-                  {lawObject[key].note.replace('(','').replace(')','')}
-                </p>
-                {hintJSX}
-              </div>
-              <div className='right floated content'>
-                {evaluateConditionJSX}
-              </div>
-            </div>
-          )
-        })
-
-      })
-    }
-
     return (
       <section className='AnswerView ui basic segment'>
         <h2 className='ui header'>
@@ -123,30 +43,12 @@ class AnswerView extends Component {
             </div>
           </div>
           <div className='left aligned column'>
-            <h4 className='ui dividing header'>掃描結果</h4>
-            <div className='ui secondary three item menu' style={{marginTop: "-1rem"}}>
-              <a className='item'>
-                通過
-                <span className='ui green mini label'>
-                  {count.passed}
-                </span>
-              </a>
-              <a className='item'>
-                不通過
-                <span className='ui red mini label'>
-                  {count.failed}
-                </span>
-              </a>
-              <a className='item'>
-                不適用
-                <span className='ui mini label'>
-                  {count.NA}
-                </span>
-              </a>
-            </div>
-            <div className='ui divided relaxed list'>
-              {resultJSX}
-            </div>
+            <ResultView
+              answerData={answerData}
+              rulesData={rulesData}
+              lawObject={lawObject}
+              lawTitle='勞動基準法'
+            />
           </div>
         </div>
       </section>
---
+++ src/answer/ResultView.js
@@ -0,0 +1,195 @@
+import React, { Component } from 'react'
+import {HashLink as Link} from 'react-router-hash-link'
+
+import _evaluateCondition from './_evaluateCondition'
+
+import './ResultView.css'
+
+class ResultView extends Component {
+
+  constructor(props) {
+
+    super(props)
+
+    this.state = {
+      filter: 'all',
+      chooser: false,
+    }
+
+    this._filter = this._filter.bind(this)
+
+  }
+
+  _filter(event) {
+
+    const filter = event.target.getAttribute('data-filter')
+    this.setState((prevState, props) => {
+      prevState.filter = filter
+      return prevState
+    })
+
+  }
+
+  _toggleChooser() {
+
+    this.setState((prevState, props) => {
+      prevState.chooser = !prevState.chooser
+      return prevState
+    })
+
+  }
+
+  render () {
+
+    const answerData = this.props.answerData
+    const rulesData = this.props.rulesData
+    const lawObject = this.props.lawObject
+    const lawTitle = this.props.lawTitle
+    let count = {
+      'all': 0,
+      'passed': 0,
+      'failed': 0,
+      'NA': 0
+    }
+
+    let resultJSX
+    if (answerData && rulesData) {
+      resultJSX = Object.keys(rulesData).map((key) => {
+
+        if (key === '0') {
+          return
+        }
+
+        return rulesData[key].map((ruleSet, index) => {
+          if (!ruleSet) {
+            return
+          }
+
+          let evaluateConditionJSX
+          let hintJSX
+
+          if (ruleSet.precondition && _evaluateCondition(ruleSet.precondition, answerData) !== 'passed') {
+
+            count.NA += 1
+            count.all += 1
+
+            if (this.state.filter !== 'all' && this.state.filter !== 'NA') {
+              return
+            } 
+
+            evaluateConditionJSX = '不適用'
+
+          } else {
+
+            const resultValue = _evaluateCondition(ruleSet.condition, answerData)
+
+            if (resultValue === 'passed') {
+              count.passed += 1
+              count.all += 1
+
+              evaluateConditionJSX = <span className='ui green basic tiny label'><i className='icon checkmark' />通過</span>
+            }
+
+            if (resultValue === 'failed') {
+              count.failed += 1
+              count.all += 1
+
+              evaluateConditionJSX = <span className='ui red tiny label'><i className='icon remove' />不通過</span>
+            }
+
+            if (this.state.filter !== 'all' && this.state.filter !== resultValue) {
+              return
+            } 
+
+            const targetListJSX = ruleSet.condition.rule.map((item, number, arr) => {
+              return (
+                <div className='item' key={number}>
+                <span className='code'>
+                  {item.target}
+                </span>
+                </div>
+              )
+            })
+
+            hintJSX = (
+              <div className='ui list'>
+                {targetListJSX}
+              </div>
+            )
+          }
+
+          return (
+            <div className='item' key={index}>
+              <div className='content'>
+                <header className='header'>
+                  {lawTitle}第 {key} 條
+                  { ruleSet.reference.paragraph.length > 0 ? `第 ${ruleSet.reference.paragraph} 項` : ''}
+                  { ruleSet.reference.subparagraph.length > 0 ? `第 ${ruleSet.reference.subparagraph} 款` : ''}
+                </header>
+                <p className='meta'>
+                  {lawObject[key].note.replace('(','').replace(')','')}
+                </p>
+                {hintJSX}
+              </div>
+              <div className='right floated content'>
+                {evaluateConditionJSX}
+              </div>
+            </div>
+          )
+        })
+
+      })
+    }
+
+    return (
+      <section className='ResultView'>
+        <h4 className='ui dividing header'
+          data-filter='all'
+          onClick={this._filter}
+        >
+          {lawTitle}掃描結果（共 {count.all} 條規則）
+        </h4>
+        <div className='ui secondary three item menu' style={{marginTop: "-1rem"}}>
+          <a className='item'
+            data-filter='passed'
+            onClick={this._filter}
+          >
+            通過
+            <span className='ui green mini label'
+              data-filter='passed'
+            >
+              {count.passed}
+            </span>
+          </a>
+          <a className='item'
+            data-filter='failed'
+            onClick={this._filter}
+          >
+            不通過
+            <span className='ui red mini label'
+              data-filter='failed'
+            >
+              {count.failed}
+            </span>
+          </a>
+          <a className='item'
+            data-filter='NA'
+            onClick={this._filter}
+          >
+            不適用
+            <span className='ui mini label'
+            data-filter='NA'
+            >
+              {count.NA}
+            </span>
+          </a>
+        </div>
+        <div className='ui divided relaxed list'>
+          {resultJSX}
+        </div>
+      </section>
+    )
+  }
+}
+
+export default ResultView
