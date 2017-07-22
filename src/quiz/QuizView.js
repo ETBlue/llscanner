a0e49e96b36630a945bb5b8f34d85e4b60d545de
ETBlue <etblue.tw@gmail.com>
2017-07-23 03:43:03 +0800

    fix homepage

--- src/quiz/QuizView.js
+++ src/quiz/QuizView.js
@@ -49,6 +49,10 @@ class QuizView extends Component {
     const orderData = this.props.orderData || {}
 
     const quizData = this.props.quizData
+    if (!quizData) {
+      return null
+    }
+
     const optionData = quizData.option
     const routeData = quizData.route
 
