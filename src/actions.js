92bc8c961b03ed14a40c3f12ca7048be2ed52b32
ETBlue <etblue.tw@gmail.com>
2017-11-02 07:09:55 +0800

    add action creators

--- src/actions.js
+++ src/actions.js
@@ -22,3 +22,23 @@ const UPDATE_QUIZ = "UPDATE_QUIZ"
 const UPDATE_STEP = "UPDATE_STEP"
 const UPDATE_RULE = "UPDATE_RULE"
 const UPDATE_ANSWER = "UPDATE_ANSWER"
+
+export function login(id) {return { type: LOGIN, id }}
+export function logout() {return { type: LOGOUT }}
+export function viewReport() {return { type: VIEW_REPORT }}
+export function viewQuizList() {return { type: VIEW_QUIZLIST }}
+export function viewStepList() {return { type: VIEW_STEPLIST }}
+export function viewLawList() {return { type: VIEW_LAWLIST }}
+export function viewRuleList(lawId) {return { type: VIEW_RULELIST, lawId }}
+export function viewQuiz(id) {return { type: VIEW_QUIZ, id }}
+export function viewStep(id) {return { type: VIEW_STEP, id }}
+export function viewRule(lawId, ruleId) {return { type: VIEW_RULE, lawId, ruleId }}
+export function addQuiz(id) {return { type: ADD_QUIZ, id }}
+export function addStep() {return { type: ADD_STEP }}
+export function deleteQuiz(id) {return { type: DELETE_QUIZ, id }}
+export function deleteStep(id) {return { type: DELETE_STEP, id }}
+export function updateQuiz(id, content) {return { type: UPDATE_QUIZ, id, content }}
+export function updateStep(id, content) {return { type: UPDATE_STEP, id, content }}
+export function updateRule(lawId, ruleId, content) {return { type: UPDATE_RULE, lawId, ruleId, content }}
+export function updateAnswer(id, content) {return { type: UPDATE_ANSWER, id, content }}
+
