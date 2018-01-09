a0a0f38a6ff90314f57df5497f5f5c117ad17bc9
ETBlue <etblue.tw@gmail.com>
2018-01-10 03:24:45 +0800

    polish option buttons

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,7 @@ import {HashLink as Link} from 'react-router-hash-link'
 import PropTypes from 'prop-types'
 
 const Option = ({title, link, onClick}) => (
-  <Link to={`/${link}`} className='Option ui button' onClick={onClick}>
+  <Link to={`/${link}`} className='Option ui button' onClick={onClick} style={{margin: '0 1rem 1rem 0'}} >
     {title}
   </Link>
 )
