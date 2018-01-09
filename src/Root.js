ac9dbfb0f1cc6010f6ae9d73a2f8d7c155844275
ETBlue <etblue.tw@gmail.com>
2018-01-10 03:30:36 +0800

    polish welcome, report page

--- src/Root.js
+++ src/Root.js
@@ -13,15 +13,28 @@ import QuizPage from './containers/QuizPage'
 import Header from './components/Header'
 
 const HomePage = () => (
-  <section>
-    <p>welcome</p>
-    <Link to={`/${StepData.basic[0]}`} className='ui button'>
-    start
+  <section className='ui basic center aligned segment'>
+    <h1 className='ui header'>某段歡迎標題</h1>
+    <p>某段歡迎文字</p>
+    <hr className='ui hidden divider' />
+    <Link to={`/${StepData.basic[0]}`} className='ui icon labeled right button'>
+      開始
+      <i className='icon right chevron'></i>
     </Link>
   </section>
 )
 
-const ReportPage = () => (<p>report</p>)
+const ReportPage = () => (
+  <section className='ui basic center aligned segment'>
+    <h1 className='ui header'>掃描報告</h1>
+    <p>某段敘述文字</p>
+    <hr className='ui hidden divider' />
+    <Link to={`/${StepData.basic[0]}`} className='ui icon labeled right button'>
+      再玩一次
+      <i className='icon right chevron'></i>
+    </Link>
+  </section>
+)
 
 const Root = () => {
 
