9673ac0453eb66a5a1f370e5c70c83af2859a67d
ETBlue <etblue.tw@gmail.com>
2018-02-09 02:59:57 +0800

    add theme music

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,19 +8,24 @@ const Footer = () => (
     Yet another open data experiment by ETBlue.
   </p>
   <p style={{opacity: '0.6'}}>
-    <a href='https://github.com/ETBlue/llscanner' target='_blank' >
+    <a href='https://github.com/ETBlue/llscanner' target='_blank' style={{margin: '0 0.5rem'}} >
     <i className='icon code'></i>
     Source code
     </a>
-    　
-    <a href='https://docs.google.com/spreadsheets/d/1Qp_U-zGJmvaXO0WA8LWRKh3Npsq5nitMxnQ7JE1KuHQ/edit?usp=sharing' target='_blank' >
+
+    <a href='https://docs.google.com/spreadsheets/d/1Qp_U-zGJmvaXO0WA8LWRKh3Npsq5nitMxnQ7JE1KuHQ/edit?usp=sharing' target='_blank' style={{margin: '0 0.5rem'}} >
     <i className='icon table'></i>
     Data
     </a>
-    　
-    <a href='https://www.facebook.com/ETBlue/media_set?set=a.10209533675351463.1073741859.1014354995&type=3' target='_blank' >
+
+    <a href='https://www.facebook.com/ETBlue/media_set?set=a.10209533675351463.1073741859.1014354995&type=3' target='_blank' style={{margin: '0 0.5rem'}} >
     <i className='icon photo'></i>
     Dev log
+    </a>
+
+    <a href='https://etblue.blogspot.tw/2017/09/new-song-labor-laws-scanner-theme-music.html' target='_blank' style={{margin: '0 0.5rem'}} >
+    <i className='icon music'></i>
+    Theme Music
     </a>
   </p>
   </section>
